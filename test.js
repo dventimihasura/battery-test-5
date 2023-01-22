040c0cb447c7e3f2b4381f48250b9e5143ff5133
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-01-22 12:21:08 -0800

    Added a threshold

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+export const options = {
+    http_req_duration: ['p(90) < 400']
+}
 export default function () {
     const query = `query {test_${Math.floor(Math.random()*__ENV.N)+1} {name}}`;
     const headers = {'Content-Type': 'application/json'};
